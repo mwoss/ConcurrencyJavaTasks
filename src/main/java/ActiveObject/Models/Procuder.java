24a7e2ce064cb228198654055ead1445907115f6
Mateusz Woś <wos.mateusz16@gmail.com>
2017-12-06 03:23:56 +0100

    Active Object - Prod and Cons models

--- src/main/java/ActiveObject/Models/Procuder.java
+++
@@ -1,9 +0,0 @@
-package ActiveObject.Models;
-
-public class Procuder extends Thread {
-
-    @Override
-    public void run() {
-
-    }
-}
--- src/main/java/ActiveObject/Models/Producer.java
+++ src/main/java/ActiveObject/Models/Producer.java
@@ -7,15 +7,14 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 
-public class Consumer extends Thread {
-
+public class Producer extends Thread {
     private Proxy proxy;
     private Random random;
     private int bufferLimit;
     private List<Future> taskList = new LinkedList<>();
 
 
-    public Consumer(Proxy proxy, int bufferLimit){
+    public Producer(Proxy proxy, int bufferLimit){
         this.proxy = proxy;
         this.random = new Random();
         this.bufferLimit = bufferLimit;
@@ -24,7 +23,7 @@ public class Consumer extends Thread {
     @Override
     public void run() {
         while(true){
-            taskList.add(proxy.consume(random.nextInt(bufferLimit / 2) + 1));
+            taskList.add(proxy.produce(random.nextInt(bufferLimit / 2) + 1));
 
             try{
                 Thread.sleep(200); //give him some time to finish job
